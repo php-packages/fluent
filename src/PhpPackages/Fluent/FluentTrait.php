06f64c22d0224a9026e8960325df9cdc6d619896
bound1ess <bound1ess.dev@gmail.com>
2015-02-03 19:25:07 +0600

    Write FluentTrait.

--- src/PhpPackages/Fluent/FluentTrait.php
+++ src/PhpPackages/Fluent/FluentTrait.php
@@ -3,21 +3,60 @@
 trait FluentTrait
 {
 
-    protected $fluent;
+    /**
+     * @var Fluent|null
+     */
+    protected $fluent = null;
 
+    /**
+     * Returns an existing instance of Fluent, or creates a new one first.
+     *
+     * @return Fluent
+     */
     public function getFluent()
     {
+        if (is_null($this->fluent)) {
+            $this->fluent = new Fluent;
+        }
+
+        return $this->fluent;
     }
 
+    /**
+     * Replaces the currently used Fluent instance with the given one.
+     *
+     * @param Fluent $fluent
+     * @return void
+     */
     public function setFluent(Fluent $fluent)
     {
+        $this->fluent = $fluent;
     }
 
+    /**
+     * Magic!
+     *
+     * @param string $method
+     * @param array $arguments
+     * @return object
+     */
     public function __call($method, array $arguments)
     {
+        $this->getFluent()->registerCall($method, $arguments);
+
+        return $this;
     }
 
+    /**
+     * Magic!
+     *
+     * @param string $name
+     * @return object
+     */
     public function __get($name)
     {
+        $this->getFluent()->registerCall($name);
+
+        return $this;
     }
 }
