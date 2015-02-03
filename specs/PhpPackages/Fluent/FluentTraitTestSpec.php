ffc049c37eb21d4e824865b711cb6b4a5cd304d6
bound1ess <bound1ess.dev@gmail.com>
2015-02-03 21:20:30 +0600

    Test FluentTrait.

--- specs/PhpPackages/Fluent/FluentTraitTestSpec.php
+++ specs/PhpPackages/Fluent/FluentTraitTestSpec.php
@@ -3,12 +3,29 @@
 namespace specs\PhpPackages\Fluent
 {
 
+    use PhpPackages\Fluent\Fluent;
+
     class FluentTraitTestSpec extends \PhpSpec\ObjectBehavior
     {
 
         public function it_is_initializable()
         {
             $this->shouldHaveType("PhpPackages\Fluent\FluentTraitTest");
+        }
+
+        public function it_returns_the_existing_Fluent_instance_and_creates_a_new_one()
+        {
+            $this->getFluent()->shouldHaveType("PhpPackages\Fluent\Fluent");
+
+            $this->setFluent($fluent = new Fluent);
+            $this->getFluent()->shouldBe($fluent);
+        }
+
+        public function it_brings_in_the_magic(Fluent $fluent)
+        {
+            $this->setFluent($fluent);
+            $fluent->registerCall("doSomethingCool", [])->shouldBeCalled();
+            $this->getWrappedObject()->doSomethingCool();
         }
     }
 }
