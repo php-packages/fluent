f80b0a5c63c41bd01828ad21eade7006deecdcf8
bound1ess <bound1ess.dev@gmail.com>
2015-02-09 21:30:07 +0600

    Improve FluentSpec.php

--- specs/PhpPackages/Fluent/FluentSpec.php
+++ specs/PhpPackages/Fluent/FluentSpec.php
@@ -11,17 +11,26 @@ class FluentSpec extends \PhpSpec\ObjectBehavior
     public function it_registers_a_call()
     {
         $this->getCalls()->shouldBe([]);
+
+        $this->registerCall("type", ["string"]);
+        $this->getCalls()->shouldBe([["type", ["string"]]]);
+
+        $this->clearCalls();
+
         $this->registerCall("shouldBeEmpty");
         $this->getCalls()->shouldBe(["should", "be", "empty"]);
+
         $this->clearCalls();
 
         $this->registerCall("_should");
         $this->getCalls()->shouldBe(["should"]);
+
         $this->clearCalls();
 
-        $this->getCalls()->shouldBe([]);
         $this->registerCall("should_has_length", [8]);
         $this->getCalls()->shouldBe($calls = ["should", "has", ["length", [8]]]);
+
+        // $this->clearCalls();
 
         $this->registerCall("shouldNot_be_truthy");
         $this->getCalls()->shouldBe(array_merge($calls, ["should", "not", "be", "truthy"]));
--- src/PhpPackages/Fluent/Fluent.php
+++ src/PhpPackages/Fluent/Fluent.php
@@ -4,6 +4,8 @@ class Fluent
 {
 
     /**
+     * The registered calls.
+     *
      * @var array
      */
     protected $calls = [];
